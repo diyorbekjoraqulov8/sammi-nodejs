776d1e0dd9c6a8d1e87398b4438c87ed4b4f23db
diyorbekjoraqulov8 <diyorbekjoraqulov8@gmail.com>
2023-08-30 09:23:52 +0300

    Two version worked with html, Add smal Api

--- index.js
+++ index.js
@@ -1,36 +1,123 @@
 const http = require('http')
+const fs = require('fs')
+const path = require('path')
 
 const server = http.createServer((req, res) => {
+  // worked with HTML
   if (req.method === 'GET') {
     res.writeHead(200, {'Content-Type': 'text/html'})
-    res.end(`
-      <h2>Send name</h2>
-      <form method="post" action="/">
-        <input name="name" placeholder="Enter your name">
-        <button type="submit">Send name</button>
-      </form>
-    `)
-  } else if (req.method === 'POST') {
-    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
-    const body = []
 
+    if (req.url === '/') {
+      fs.readFile(path.join(__dirname, 'templates', 'index.html'), 'utf-8', (err, content) => {
+        if(err) throw new Error()
+
+        res.end(content)
+      })
+    } else if (req.url === '/about') {
+      fs.readFile(path.join(__dirname, 'templates', 'about.html'), 'utf-8', (err, content) => {
+        if(err) throw new Error()
+
+        res.end(content)
+      })
+    } else if (req.url === '/contact') {
+      fs.readFile(path.join(__dirname, 'templates', 'contact.html'), 'utf-8', (err, content) => {
+        if(err) throw new Error()
+
+        res.end(content)
+      })
+    } else if (req.url === '/api/admin') {
+      // Add Api
+      res.writeHead(200, {'Content-Type': 'text/json'})
+
+      const admin = {name:"Diyorbek", surname:'Juraqulov', job: "FullStack developer"}
+
+      res.end(JSON.stringify(admin))
+    }
+
+
+
+    // res.writeHead(200, {'Content-Type': 'text/html'})
+    // if (req.url === '/') {
+    //   fs.readFile(path.join(__dirname, 'templates', 'index.html'), 'utf-8' , (err, content) => {
+    //     if (err) throw new Error()
+    //     res.end(content)
+    //   })
+    // } else if (req.url === '/about') {
+    //   fs.readFile(path.join(__dirname, 'templates', 'about.html'), 'utf-8' , (err, content) => {
+    //     if (err) throw new Error()
+    //     res.end(content)
+    //   })
+    // } else if (req.url === '/contact') {
+    //   fs.readFile(path.join(__dirname, 'templates', 'contact.html'), 'utf-8', (err, content) => {
+    //     if (err) throw new Error()
+    //     res.end(content)
+    //   })
+    // }
+
+  } else if (req.method === 'POST') {
+    res.writeHead(200, {'Content-Type': 'text/html'})
+
+    const body = []
     req.on('data', data => {
       body.push(Buffer.from(data))
-      
-      console.log(body.toString())
+
+      console.log(body.toString());
     })
 
     req.on('end', () => {
-      const message = body.toString().split("=")[1]
+      const message = body.toString().split('=')[1]
 
-      res.end(`<h2>Name succesfully added: ${message}</h2>`)
+      res.end(`
+        <h2>Name added succesfully: ${message}</h2>
+        <a href="/">Go to home</a>
+      `)
     })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
+    // const body = []
+
+    // req.on('data', data => {
+    //   body.push(Buffer.from(data))
+      
+    //   console.log(body.toString())
+    // })
+
+    // req.on('end', () => {
+    //   const message = body.toString().split("=")[1]
+
+    //   res.end(`<h2>Name succesfully added: ${message}</h2>`)
+    // })
   }
 
-
   console.log(req.method);
-
-  
 })
 
 server.listen(3000, () => {
